79b6fed05ece90d417984bc8ba13d3d7a50913cf
shashwatmacharya <120778674+shashwatmacharya@users.noreply.github.com>
2023-04-21 10:29:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let input_city = document.getElementById('cityinput');
+let input_btn = document.getElementById('btn');
+let output_city = document.getElementById('city');
+let output_temp = document.getElementById('temp');
+let output_desc = document.getElementById('desc');
+let od_key="641d8a6132c2f05d70e36e04bdf9f063";
+
+function convert(value){
+    let t=(value-273).toFixed(2)
+    return t;
+}
+function convertion(val){
+    return (val - 273).toFixed(2)
+}
+
+input_btn.addEventListener('click',()=>{
+    //display temp of city
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+input_city.value+'&appid='+od_key)
+    .then(Response => Response.json())
+    .then(data =>{
+        console.log(data);
+        output_city.innerHTML='City: '+data.name;
+        output_temp.innerHTML='Temperature: '+convert(data.main.temp)+' Celcius';
+        output_desc.innerHTML='Description: '+data.weather[0].description;
+    })
+    .catch(error => console.log('there is an error'+error))
+})
+
